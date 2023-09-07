127f6daadf07a402382d7c4ed94acdc77a855fa7
Eric <ericmabr@gmail.com>
2023-09-06 21:18:51 -0400

    Basic input framework done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  // State: when 
+
+  // State var
+  const [originLoc, setOriginLoc] = useState('');
+  const [destLoc, setDestLoc] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [returnDate, setReturnDate] = useState('');
+
+
+  // Function to handle changes in the input field
+  const handleOriginLocChange = (event) => {
+    setOriginLoc(event.target.value);
+  };
+  // Function to handle changes in the input field
+  const handleDestLocChange = (event) => {
+    setDestLoc(event.target.value);
+  };
+  const handleStartDateChange = (event) => {
+    setStartDate(event.target.value);
+  };
+  const handleReturnDateChange = (event) => {
+    setReturnDate(event.target.value);
+  };
+
+
+
+  const handleSearch = () => {
+    // Call your search function with the search term
+    // Replace this with your actual search logic
+    console.log('Origin:', originLoc);
+    console.log('Destination:', destLoc);
+    console.log('Start Date:', startDate);
+    console.log('Return Date:', returnDate);
+
+  };
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <main className='App-main'>
+        <h1>
+          Fly Broke :/
+        </h1>
+
+        <div className='input-bubble'>
+          <input
+            type="text"
+            placeholder="Origin..."
+            value={originLoc}
+            onChange={handleOriginLocChange}
+          />
+
+          <input
+            type="text"
+            placeholder="Destination..."
+            value={destLoc}
+            onChange={handleDestLocChange}
+          />
+
+          <label for="start">Start date:</label>
+          <input
+            type="date"
+            id="start"
+            name="trip-start"
+            value={startDate}
+            onChange={handleStartDateChange}
+          />
+          <input
+            type="date"
+            id="end"
+            name="trip-end"
+            value={returnDate}
+            onChange={handleReturnDateChange}
+          />
+        </div>
+
+        <button onClick={handleSearch}>Search</button>
+
+      </main>
+
+      <body>
+        <div className='listnode-container'>
+
+        </div>
+      </body>
     </div>
   );
 }
